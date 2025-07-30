0bc2aba4555e8d3356a161d012e411fe5abad753
Andrew Lima <andrew@arctek.co.za>
2025-07-30 12:37:09 +0200

    Adjust code to support PHP 7.2

--- Providers/PmproFreescoutServiceProvider.php
+++ Providers/PmproFreescoutServiceProvider.php
@@ -166,11 +166,11 @@ class PmproFreescoutServiceProvider extends ServiceProvider
 
 		// Reorder the mailbox folders, maintain the default folders structure and squeeze in custom folders above the default. Default folders are 'type' 1-80, custom folders are 81+.
 		\Eventy::addFilter('mailbox.folders', function ($folders, $mailbox) {
-			return $folders
-			->filter(fn($folder) => $folder->type > 80)  // Pull high-priority types first
-			->concat($folders->filter(fn($folder) => $folder->type <= 80)) // Append the rest as-is
-			->values();
-		}, 90, 2);
+            return $folders
+            ->filter(function($folder) { return $folder->type > 80; })  // Pull high-priority types first
+            ->concat($folders->filter(function($folder) { return $folder->type <= 80; })) // Append the rest as-is
+            ->values();
+        }, 90, 2);
 	}
 
     /**
